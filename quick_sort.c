f0d0571ba0a735f943838af572ab3cde121ada4b
Aero <1307386212@qq.com>
2024-12-23 23:28:41 +0800

    Array exercise.

--- quick_sort.c
+++ quick_sort.c
@@ -13,20 +13,34 @@ void SwapElements(int Array[], int First, int Second)
   Array[Second] = temp;
 }
 
+//void test_rand(void)
+//{
+//  unsigned long n;
+//  srand((unsigned)time(NULL));     //随机数测试代码
+//  for(int i = 0; i < 100; i++)
+//  {
+//    n = rand();
+//    printf("%d\n", n);
+//  }
+//}
+
 //打乱数组，第二种方式
 void ShuffleArray(int Array[], int Length)
 {
-  srand(time(NULL));
+  srand((unsigned)time(NULL));
   for (int i = Length -i; i > 0; --i) {
-    int Random_Number = rand() % i;
-    SwapElements(Array, i, Random_Number);
+    int a= rand();            //修改为直接将随机数插入数组
+    Array[i] = a;
+    //int Random_Number = rand() % i;
+    //SwapElements(Array, i, Random_Number);
   }
 }
 
+//快速排序
 void quick_sort(int arr[], int left, int right)
 {
-  if (left >= right) return;
-  int i = left, medium = arr[left], j = right;
+  if (left >= right) return;   //判断左右指针是否到达搜索边界
+  int i = left, medium = arr[left], j = right;       // 定义左右界及中位数
   while (i < j)
   {
     while (arr[i] < medium) i++;
@@ -38,7 +52,7 @@ void quick_sort(int arr[], int left, int right)
           SwapElements(arr, i, j);
     }
   }
-  quick_sort(arr, left, j - 1);
+  quick_sort(arr, left, j - 1);         //递归
   quick_sort(arr, j + 1, right);
 }
 
@@ -63,14 +77,15 @@ void quick_sort(int arr[], int left, int right)
 
 int main()
 {
-  int Players[50] = {0};
+  int Players[50] = {0};             //新建数组并初始化
   for (int i = 0; i < 50; i++)
   {
-    Players[i] = i;
+    Players[i] = i;                   //插入数字
   }
-  ShuffleArray(Players, 50);
-  PRINT_INT_ARRAY(Players, 50);
-  quick_sort(Players, 0, 49);
+
+  ShuffleArray(Players, 50);          //打乱数组
+  PRINT_INT_ARRAY(Players, 50);    //打印数组
+  quick_sort(Players, 0, 49);      //快排
   PRINT_INT_ARRAY(Players, 50);
   return 0;
 }
